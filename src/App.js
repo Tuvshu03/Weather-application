5dfe943b79a3ea2c7bc4aaf6fb31bdc836478c08
Tuvshu03 <159090318+Tuvshu03@users.noreply.github.com>
2025-01-13 13:16:08 +0800

    search input completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,73 @@
+import React, { useEffect, useState } from "react";
 
+const citiesFilter = (countries) => {
+  const citiesAndCountries = countries.flatMap((country) => {
+    return country.cities.map((city) => {
+      return `${city}, ${country.country}`;
+    });
+  });
+  return citiesAndCountries;
+};
 
 function App() {
+  const [citiesSearch, setCitiesSearch] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [cities, setCities] = useState([]);
+
+  const handleChange = (event) => {
+    setCitiesSearch(event.target.value);
+  };
+
+  const fetchData = async () => {
+    setLoading(true);
+    await fetch(" https://countriesnow.space/api/v0.1/countries")
+      .then((response) => response.json())
+      .then((result) => {
+        const citiesAndCountries = citiesFilter(result.data);
+
+        setCities(citiesAndCountries);
+        setFilteredData(citiesAndCountries);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const filterData = () =>
+    setFilteredData(
+      cities
+        .filter((city) =>
+          city.toLowerCase().startsWith(citiesSearch.toLowerCase())
+        )
+        .slice(0, 5)
+    );
+
+  useEffect(() => {
+    console.log("useEffect fetch data worked");
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    filterData();
+  }, [citiesSearch]);
+
   return (
-    <div className="App">
-      <div class="text-3xl font-bold underline">hello world</div>
+    <div class="__className_43d6c0 bg-[#F3F4F6]">
+      
+      <div>
+        <input onChange={handleChange} placeholder="Search city"></input>
+      </div>
+      {loading && <p>loading...</p>}
+      <div>
+        {citiesSearch.length > 0 &&
+          filteredData.map((country, index) => {
+            return <div key={index}>{country}</div>;
+          })}
+      </div>
     </div>
   );
 }
